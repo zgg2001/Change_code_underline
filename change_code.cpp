4fae8fa980dd7ebadc37f0374d8cedb52ec35655
zgg2001 <397606244@qq.com>
2021-09-16 23:13:51 +0800

    继承基类接口 设计流程框架 开发读取文件部分

--- change_code.cpp
+++ change_code.cpp
@@ -10,9 +10,16 @@
 change_code::change_code(int argc, char* argv[])
 {
     //如果参数数量不对则输出help信息
-    if(help_judge(argc) == false)
+    if(_argument_number_judge(argc) == false)
     {
-        help_print();
+        _state = false;
+        _argument_number_error_print();
+    }
+    else
+    {
+        //确定输入输出文件
+        set_input_file(argv[1]);
+        set_output_file(argv[2]);
     }
 }
 
@@ -21,12 +28,83 @@ change_code::~change_code()
     
 }
 
-void change_code::help_print()
+bool change_code::change()
+{
+    //初始化失败
+    if(_state == false)
+    {
+        cout << "初始化失败，无法转换" << endl;
+        return false;
+    }
+    //读取文件至缓冲区
+    if(_read_content_to_map() == false)
+    {
+        _input_file_error_print();
+        return false;
+    }
+    //处理缓冲区内的内容
+    if(_handle_content_from_map() == false)
+    {
+        return false;
+    }
+    //将处理后的内容输出至文件
+    if(_write_content_to_file() == false)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool change_code::_read_content_to_map()
+{
+    ifstream infile;
+    infile.open(get_input_file(), ios::binary);
+    
+    if(infile.is_open())
+    {
+        //获取文件内容
+        string temp;
+        while(getline(infile, temp))
+        {
+            cout << temp << endl;
+        }
+        infile.close();
+    }
+    else
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+bool change_code::_handle_content_from_map()
+{
+    return true;
+}
+
+bool change_code::_write_content_to_file()
+{
+    return true;
+}
+
+void change_code::_argument_number_error_print()
 {
     cout << endl;
     cout << "欢迎使用本代码混淆器" << endl;
-    cout << "by ZHJ" << endl;
-    cout << "使用格式如下:" << endl;
+    cout << "by ZHJ" << endl << endl;
+    cout << "请确认参数格式是否正确:" << endl;
     cout << "./line [原文件] [混淆后文件]" << endl;
     cout << endl;
 }
+
+void change_code::_input_file_error_print()
+{
+    cout << endl;
+    cout << "原文件读取失败" << endl;
+    cout << "请确认原文件路径是否正确" << endl << endl;
+    cout << "参数格式如下:" << endl;
+    cout << "./line [原文件] [混淆后文件]" << endl;
+    cout << endl;    
+}
+
--- change_code.h
+++ change_code.h
@@ -9,20 +9,38 @@
 #define _CHANGE_CODE_H_
 
 #include<iostream>
+#include<fstream>
+#include"change_code_base.h"
 
 using namespace std;
 
-class change_code
+class change_code : public _change_code_base
 {
 public:
     change_code(int argc, char* argv[]);
     virtual ~change_code();
+    //公有方法 开始转换
+    bool change();
 
-public:
-    bool help_judge(int argc) { return argc == 3; } //判定参数数量
-    void help_print(); //当参数有误时输出提示信息
-    
+protected:
+    //读取文件至缓冲区
+    virtual bool _read_content_to_map();
+    //处理缓冲区内的内容
+    virtual bool _handle_content_from_map();
+    //将处理后的内容输出至文件
+    virtual bool _write_content_to_file();
+
+protected:
+    //判定参数数量
+    bool _argument_number_judge(int argc) { return argc == 3; }
+    //当参数有误时输出提示信息
+    virtual void _argument_number_error_print();
+    //输入文件打开读取出现问题的提示
+    virtual void _input_file_error_print();
+
 private:
+    //当此值为false时 说明初始化失败
+    bool _state = true;
 
 };
 
