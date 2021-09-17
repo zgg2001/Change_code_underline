580164768a016b2854a2d4cef06c7ba5b9a263d4
zgg2001 <397606244@qq.com>
2021-09-17 21:31:33 +0800

    混淆功能已经初步实现

--- change_code.h
+++ change_code.h
@@ -10,6 +10,8 @@
 
 #include<iostream>
 #include<fstream>
+#include<vector>
+#include<unordered_map>
 #include"change_code_base.h"
 
 using namespace std;
@@ -31,17 +33,36 @@ protected:
     virtual bool _write_content_to_file();
 
 protected:
+    //改变字符串内容
+    virtual bool _change_content(string& temp);
+    //判定当前字符是否为字母、数字、下划线
+    bool _judge_number_letter_underline(char c);
+    //进行单词与下划线之间的转换
+    bool _change_word_to_underline(const string& word);
+
+protected:
     //判定参数数量
-    bool _argument_number_judge(int argc) { return argc == 3; }
+    virtual bool _argument_number_judge(int argc) { return argc == 3; }
     //当参数有误时输出提示信息
     virtual void _argument_number_error_print();
     //输入文件打开读取出现问题的提示
     virtual void _input_file_error_print();
+    //处理内容时出现问题
+    virtual void _handle_content_error_print();
+    //输出内容时出现问题
+    virtual void _write_content_error_print();
 
 private:
     //当此值为false时 说明初始化失败
     bool _state = true;
-
+    //储存文件中的内容
+    vector<string> _content;
+    //储存define语句
+    vector<string> _defines;
+    //储存单词map
+    unordered_map<string, string> _words;
+    //储存当前下划线长度
+    string _underline = "";
 };
 
 #endif
